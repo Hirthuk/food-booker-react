0bc941f7498d1d3a79b5fd9448dd64b66d83bb9e
Hirthuk <sharankumarp02@gmail.com>
2025-05-19 11:50:48 +0530

    Started

---
+++ src/assets/assest.js
@@ -0,0 +1,6 @@
+import Logo from './FoodBooker_Logo.png'
+import Hero_Backgroundimage from './Hero_background.png'
+export const  assets=  {
+    Logo,
+    Hero_Backgroundimage
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {assets} from '../assets/assest'
+const Navbar = () => {
+  return (
+    <header>
+     
+        <div className='flex flex-col justify-between'>
+        <h1>Sharan</h1>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
