4af628ec481f647c21df8eea3b22f44668a78f3c
Hirthuk <sharankumarp02@gmail.com>
2025-06-01 19:26:30 +0530

    BUild update

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ import {
     MyCart,
     Trending,
 } from './pages'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import SearchBar from './components/SearchBar'
-import PageNotFound from './components/pagenotfound'
-import ShopItem from './pages/ShopItem'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import Profile from './pages/Profile'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import SearchBar from './components/SearchBar.jsx'
+import NotFound from './components/NotFound.jsx'
+import ShopItem from './pages/ShopItem.jsx'
+import Login from './components/Login.jsx'
+import Signup from './components/SignUp.jsx'
+import Profile from './pages/Profile.jsx'
 const App = () => {
   return (
     <div className="px-2 sm:px-[2vw] md:px-[3vw] lg:px-[4vw]  mt-0">
@@ -35,7 +35,7 @@ const App = () => {
         <Route path='shop/:id' element={<ShopItem/>}/>
         <Route path='login' element = {<Login/>}/>
         <Route path='signup' element = {<Signup/>} />
-        <Route path="*" element={<PageNotFound/>} />
+        <Route path="*" element={<NotFound/>} />
         <Route path='profile' element = {<Profile/>} />
       </Routes>
       <Footer/>
